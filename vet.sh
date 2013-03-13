4a63bb93e3d7350b27bb27e4c28d811dd5fc7546
Ryan Uber <ru@ryanuber.com>
2013-03-12 17:38:43 -0700

    updating logic in vet.sh for ip4

--- vet.sh
+++ vet.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 function vet_ip4()
 {
-    [[ "$1" =~ ^([1-9][0-9]{0,2}\.){3}[1-9][0-9]{0,2}$ ]] || return 1
+    [[ "$1" =~ ^[1-9]([0-9]){0,2}(\.[0-9]{1,3}){3}$ ]] || return 1
     for OCT in ${1//\./ }; do
+        [ ${#OCT} -gt 1 ] && [ ${OCT:0:1} -eq 0 ] && return 1
         [ $OCT -lt 256 ] || return 1
     done
     return 0
