fe5259e2240efe4ef6bea88ce959bb6d76c796d7
Ryan Uber <ru@ryanuber.com>
2013-03-13 23:42:57 -0700

    Adding initial Python code parser

--- CheckIt.rb
+++ CheckIt.rb
@@ -19,9 +19,9 @@ module CheckIt
 
   def render(file)
     check = YAML::load(File.open(file))
-    puts self.open_function(check['name'])+"\n"
+    print self.open_function(check['name'])+"\n"
     if check.has_key?('regex')
-      puts self.regex(@input_var, check['regex'])+"\n"
+      print self.regex(@input_var, check['regex'])+"\n"
     end
     if check.has_key?('min')
       puts self.min(@input_var, check['min'])+"\n"
@@ -158,7 +158,75 @@ module CheckIt
     end
   end
 
+  module Python
+    extend CheckIt
+    extend self
+
+    @indent_str = '    '
+    @indent_lvl = 0
+    @input_var = 'input'
+    @chunk_var = 'chunk'
+
+    def open_function(name)
+      result = self.indent_more "def is_#{name}(input):\n"
+      result += self.indent "from re import match"
+      result
+    end
+
+    def close_function
+      ""
+    end
+
+    def regex(var_name, regex)
+      result = self.indent_more "if not match('#{regex}', #{var_name}):\n"
+      result += self.indent "return False"
+      self.indent_less ""
+      result
+    end
+
+    def open_chunk_loop(split_by)
+      self.indent_more "for #{@chunk_var} in #{@input_var}.split(\"#{split_by}\"):"
+    end
+
+    def close_chunk_loop
+      self.indent_less ""
+    end
+
+    def min(var_name, min)
+      result = self.indent_more "if int(#{var_name}) < #{min}:\n"
+      result += self.indent "return False"
+      self.indent_less ""
+      result
+    end
+
+    def max(var_name, max)
+      result = self.indent_more "if int(#{var_name}) > #{max}:\n"
+      result += self.indent "return False"
+      self.indent_less ""
+      result
+    end
+
+    def min_len(var_name, min_len)
+      result = self.indent_more "if len(#{var_name}) < #{min_len}:\n"
+      result += self.indent "return False"
+      self.indent_less ""
+      result
+    end
+
+    def max_len(var_name, max_len)
+      result = self.indent_more "if len(#{var_name}) > #{max_len}:\n"
+      result += self.indent "return False"
+      self.indent_less ""
+      result
+    end
+
+    def return_success
+      self.indent "return True"
+    end
+  end
+
 end
 
 CheckIt::Bash.render('checks/ip4.yml')
 CheckIt::Ruby.render('checks/ip4.yml')
+CheckIt::Python.render('checks/ip4.yml')
