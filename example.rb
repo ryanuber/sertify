083be1025cad28957387b4fc2da4d669c9add334
Ryan Uber <ru@ryanuber.com>
2013-04-11 21:13:03 -0700

    Adding puts statements to make running the example clearer.

--- example.rb
+++ example.rb
@@ -1,5 +1,10 @@
 require 'lib/CheckLib'
 
-CheckLib::Bash.render('checks/ip4.yml')
-CheckLib::Ruby.render('checks/ip4.yml')
-CheckLib::Python.render('checks/ip4.yml')
+puts "\nGenerating bash code...\n"
+puts CheckLib::Bash.render('checks/ip4.yml')
+
+puts "\nGenerating ruby code...\n"
+puts CheckLib::Ruby.render('checks/ip4.yml')
+
+puts "\nGenerating python code...\n"
+puts CheckLib::Python.render('checks/ip4.yml')
